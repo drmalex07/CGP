c908daf4b112d75e5711eeab678f72a8c883c1f8
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-12-10 13:13:26 +0200

    Tweak title, legend for graphs generated by curl plugin

--- plugin/curl.php
+++ plugin/curl.php
@@ -3,20 +3,20 @@
 # Collectd Df plugin
 
 require_once 'conf/common.inc.php';
-require_once 'type/GenericStacked.class.php';
+require_once 'type/Default.class.php';
 require_once 'inc/collectd.inc.php';
 
 # LAYOUT
 #
 # curl-X/response_time.rrd
 
-$obj = new Type_GenericStacked($CONFIG);
+$obj = new Type_Default($CONFIG);
 $obj->data_sources = array('value');
 
 switch($obj->args['type']) {
 	case 'response_time':
-		$obj->rrd_title = sprintf('Response Time (%s)', $obj->args['pinstance']);
-		$obj->rrd_vertical = 'Time';
+		$obj->rrd_title = sprintf('%s', $obj->args['pinstance']);
+		$obj->rrd_vertical = 'Response Time (s)';
 		$obj->rrd_format = '%.1lf%s';
 		break;
 }
